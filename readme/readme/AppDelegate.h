439d84397d5372b20cb40b1e7fbd49479a9c357a
SMarkk <18119501227@163.com>
2015-10-21 21:21:44 +0800

    read  me  ok

---
+++ readme/readme/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  readme
+//
+//  Created by 刘委 on 15/10/21.
+//  Copyright (c) 2015年 刘委. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ readme/readme/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  readme
+//
+//  Created by 刘委 on 15/10/21.
+//  Copyright (c) 2015年 刘委. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
